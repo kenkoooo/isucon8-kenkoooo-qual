bb52cef319ebf341f242befc1034e647b03aa7c4
Kenkou Nakamura <knakamura@indeed.com>
2019-09-02 15:03:47 +0900

    stucked

--- webapp/go/src/torb/event.go
+++ webapp/go/src/torb/event.go
@@ -1,17 +1,15 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"sort"
+)
 
 func getEvent(eventID, loginUserID int64) (*Event, error) {
 	var event Event
 	if err := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID).Scan(&event.ID, &event.Title, &event.PublicFg, &event.ClosedFg, &event.Price); err != nil {
 		return nil, err
-	}
-	event.Sheets = map[string]*Sheets{
-		"S": &Sheets{},
-		"A": &Sheets{},
-		"B": &Sheets{},
-		"C": &Sheets{},
 	}
 
 	query := `
@@ -33,6 +31,9 @@ func getEvent(eventID, loginUserID int64) (*Event, error) {
 	}
 	defer rows.Close()
 
+	var sheets []*Sheet
+	var reservations []*Reservation
+	var userIDs []*sql.NullInt64
 	for rows.Next() {
 		var s Sheet
 		var r Reservation
@@ -44,12 +45,32 @@ func getEvent(eventID, loginUserID int64) (*Event, error) {
 			&s.ID, &s.Rank, &s.Num, &s.Price, &params.UserID, &r.ReservedAt); err != nil {
 			return nil, err
 		}
+		sheets = append(sheets, &s)
+		reservations = append(reservations, &r)
+		userIDs = append(userIDs, &params.UserID)
+	}
+
+	RefineEvent(reservations, sheets, userIDs, &event, loginUserID)
+
+	return &event, nil
+}
+
+func RefineEvent(reservations []*Reservation, sheets []*Sheet, userIDs []*sql.NullInt64, event *Event, loginUserID int64) {
+	event.Sheets = map[string]*Sheets{
+		"S": &Sheets{},
+		"A": &Sheets{},
+		"B": &Sheets{},
+		"C": &Sheets{},
+	}
+	for i, r := range reservations {
+		s := sheets[i]
+		userID := userIDs[i]
 		event.Sheets[s.Rank].Price = event.Price + s.Price
 		event.Total++
 		event.Sheets[s.Rank].Total++
 
 		if r.ReservedAt != nil {
-			s.Mine = params.UserID.Valid && params.UserID.Int64 == loginUserID
+			s.Mine = userID.Valid && userID.Int64 == loginUserID
 			s.Reserved = true
 			s.ReservedAtUnix = r.ReservedAt.Unix()
 		} else {
@@ -57,45 +78,107 @@ func getEvent(eventID, loginUserID int64) (*Event, error) {
 			event.Sheets[s.Rank].Remains++
 		}
 
-		event.Sheets[s.Rank].Detail = append(event.Sheets[s.Rank].Detail, &s)
+		event.Sheets[s.Rank].Detail = append(event.Sheets[s.Rank].Detail, s)
 	}
-
-	return &event, nil
 }
 
 func getEvents(all bool) ([]*Event, error) {
-	tx, err := db.Begin()
-	if err != nil {
-		return nil, err
-	}
-	defer tx.Commit()
-
-	rows, err := tx.Query("SELECT * FROM events ORDER BY id ASC")
+	query := `
+	SELECT s.*, r.user_id, r.reserved_at, e.id, e.title, e.public_fg, e.closed_fg, e.price
+	FROM events e
+	LEFT JOIN (
+		SELECT sheet_id, user_id, reserved_at, event_id
+		FROM reservations 
+		WHERE canceled_at IS NULL 
+		GROUP BY event_id, sheet_id 
+		HAVING reserved_at = MIN(reserved_at)
+	) r ON r.event_id = e.id
+	LEFT JOIN sheets s ON s.id = r.sheet_id
+	ORDER BY s.rank, s.num
+	`
+	rows, err := db.Query(query)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var events []*Event
+	eventMap := make(map[int64]Event)
+	sheets := make(map[int64][]*Sheet)
+	reservations := make(map[int64][]*Reservation)
+	userIDs := make(map[int64][]*sql.NullInt64)
 	for rows.Next() {
-		var event Event
-		if err := rows.Scan(&event.ID, &event.Title, &event.PublicFg, &event.ClosedFg, &event.Price); err != nil {
+		var e Event
+		var r Reservation
+		var userID sql.NullInt64
+		var fakeS struct {
+			ID    sql.NullInt64
+			Rank  sql.NullString
+			Num   sql.NullInt64
+			Price sql.NullInt64
+		}
+
+		if err := rows.Scan(
+			&fakeS.ID, &fakeS.Rank, &fakeS.Num, &fakeS.Price,
+			&userID, &r.ReservedAt,
+			&e.ID, &e.Title, &e.PublicFg, &e.ClosedFg, &e.Price); err != nil {
 			return nil, err
 		}
-		if !all && !event.PublicFg {
+		if !all && !e.PublicFg {
 			continue
 		}
-		events = append(events, &event)
-	}
-	for i, v := range events {
-		event, err := getEvent(v.ID, -1)
-		if err != nil {
-			return nil, err
+
+		eventMap[e.ID] = e
+		if fakeS.ID.Valid {
+			s := Sheet{
+				ID:    fakeS.ID.Int64,
+				Rank:  fakeS.Rank.String,
+				Num:   fakeS.Num.Int64,
+				Price: fakeS.Price.Int64,
+			}
+
+			sheets[e.ID] = append(sheets[e.ID], &s)
+			reservations[e.ID] = append(reservations[e.ID], &r)
+			userIDs[e.ID] = append(userIDs[e.ID], &userID)
 		}
+	}
+
+	fmt.Println(eventMap)
+	var events Events
+	for _, event := range eventMap {
+		s := sheets[event.ID]
+		r := reservations[event.ID]
+		userID := userIDs[event.ID]
+		RefineEvent(r, s, userID, &event, -1)
 		for k := range event.Sheets {
 			event.Sheets[k].Detail = nil
 		}
-		events[i] = event
+		events = append(events, &event)
 	}
+
+	//for i, v := range events {
+	//	event, err := getEvent(v.ID, -1)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	for k := range event.Sheets {
+	//		event.Sheets[k].Detail = nil
+	//	}
+	//	events[i] = event
+	//}
+	sort.Sort(events)
 	return events, nil
+}
+
+type Events []*Event
+
+func (e Events) Len() int {
+	return len(e)
+}
+
+func (e Events) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+func (e Events) Less(i, j int) bool {
+	return e[i].ID < e[j].ID
 }
